a019de26a6358494a82ee9c494b1b5f9134c97c5
可圭共 <keguigong@gmail.com>
2023-02-26 18:54:41 +0800

    Fixed Component definition is missing display name react/display-name

--- components/result/loading.tsx
+++ components/result/loading.tsx
@@ -1,7 +1,8 @@
 import { forwardRef, Ref } from "react"
 import styles from "./loading.module.scss"
 
-export const Loading = forwardRef<HTMLDivElement, {}>((props, ref) => {
+// Read more: https://stackoverflow.com/questions/71633713/error-component-definition-is-missing-display-name-react-display-name-with-reac
+export const Loading = forwardRef<HTMLDivElement, {}>(function LoadingRaw(props, ref) {
   return (
     <div ref={ref} className={styles.loadingContainer}>
       <picture>
