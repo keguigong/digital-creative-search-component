017ac894c48e3465747d6dbdb83afca38c88de90
keguigong <keguigong@gmail.com>
2023-11-01 10:54:29 +0800

    Update asset prefix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-const basePath = process.env.NODE_ENV === "production" ? "/digital-creative-search-component" : ""
-const assetPrefix =
-  process.env.NODE_ENV === "production" ? "/digital-creative-search-component" : "/"
+const basePath = process.env.NODE_ENV === "production" ? "/search-box-component" : ""
+const assetPrefix = process.env.NODE_ENV === "production" ? "/search-box-component" : "/"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // basePath: basePath,
-  // assetPrefix: assetPrefix
+  basePath: basePath,
+  assetPrefix: assetPrefix
 }
 
 module.exports = nextConfig
